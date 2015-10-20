3775e4a7ee45356916bf7c867b935245ed56774f
Tahara Kazuki <ssp0727.lnc@gmail.com>
2015-10-20 10:05:56 +0900

    KisoKadai3（文字化けする場合：文字コードの変換をお願いいたします。）

--- src/KisoKadai3.java
+++ src/KisoKadai3.java
@@ -7,58 +7,68 @@ public class KisoKadai3 {
 			BufferedReader br = new BufferedReader(is);
 
 			System.out.println("ファイル名を指定してください");
-			System.out.println("例：text ※テキスト形式でファイルを作成します。");
+			System.out.println("例：c:/Users//internous//text.txt");
 
 			String str = br.readLine();
 			File file = new File(str);
 
-			File newfile = new File(file + ".txt");
-		
+			// File newfile = new File(file);
 			try {
-				if (newfile.createNewFile()) {
+				if (file.createNewFile()) {
 					System.out.println("ファイルの作成に成功しました。");
-					System.out.println("ファイルに書き込みを行う場合"
-							+ "【1】　追記する場合【2】　参照する場合【3】　終了する場合【4】を押してください。");
+
 				} else {
 					System.out.println("既にそのファイルは存在します。");
-					System.out.println("ファイルに書き込みを行う場合"
-							+ "【1】　追記する場合【2】　参照する場合【3】　終了する場合【4】を押してください。");
-					}
-				
+				}
 			} catch (IOException e) {
 				System.out.println("例外が発生しました。再実行してください。");
 				System.out.println(e);
 			}
-			//コマンド（数字）選択した後の動作
-			String key = br.readLine();
-			
-			switch (key) {
-			case "1"://書き込みを選択した場合。
-				try{
-					  File file2 = new File(newfile+".txt");
-					  FileWriter filewriter = new FileWriter(file2);
-					  filewriter.write(br.readLine());
-					  filewriter.close();
-					}catch(IOException e){
-					  System.out.println(e);
+
+			int key = 0;
+			while (4 != key) {
+				System.out.println("ファイルに書き込みを行う場合" + "【1】　追記する場合【2】　参照する場合【3】　終了する場合【4】を押してください。");
+				// コマンド（数字）選択した後の動作
+				key = Integer.parseInt(br.readLine());
+
+				switch (key) {
+				case 1:// 書き込みを選択した場合---------------------------------------------
+					try {
+						// File file2 = new File(newfile);
+						FileWriter filewriter = new FileWriter(file);
+						filewriter.write(br.readLine() + "\r\n");
+						filewriter.close();
+					} catch (IOException e) {
+						System.out.println(e);
 					}
+					break;
+                       //----------------------------------------------------------------
 					
-			case "2"://既存ファイルへの追記を選択した場合。
-				try{
-					  File file3 = new File(newfile+".txt");
-					  FileWriter filewriter = new FileWriter(file3,true);
-					  filewriter.write(br.readLine());
-					  filewriter.close();
-					}catch(IOException e){
-					  System.out.println(e);
+				case 2:// 既存ファイルへの追記を選択した場合--------------------------------------
+					try {
+						// File file3 = new File(file+".txt");
+						FileWriter filewriter = new FileWriter(file, true);
+						filewriter.write(br.readLine() + "\r\n");
+						filewriter.close();
+					} catch (IOException e) {
+						System.out.println(e);
 					}
-						
-			case "3"://参照するを選択した場合。	
-				
-			case "5"://終了するを選択した場合。	
-				break;
+					break;
+                       //---------------------------------------------------------------
+					
+				case 3:// 参照するを選択した場合---------------------------------------------
+try {
+	
+} catch (Exception e) {
+	
+}
+				case 4:// 終了するを選択した場合---------------------------------------------
+					break;
+				default:
+					System.out.println("1～4以外が入力されています。");
+					break;
+				}
 			}
 		}
-		
-		}
 	}
+}
